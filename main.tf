dcce492f946c98f6f6ed3396d251911fe20f8822
SNEHA JG <snehajg1212@gmail.com>
2025-11-04 16:01:34 +0000

    Network

---
+++ main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+    region = "us-west-2"
+  
+}
+
+
+resource "aws_security_group" "key_security_group" {
+     ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]//allow http from anywhere
+}
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]//allow ssh from anywhere
+}
+ ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]//allow https from anywhere
+}
+    egress {
+        from_port = 0
+        to_port =  0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]//allow all outgoing traffic
+    } 
+}
+resource "aws_key_pair" "sshkey" {
+    key_name = "sshkey"
+    public_key = file("sa.pub")
+  
+}
+resource "aws_instance" "key_server" {
+    ami = "ami-0c5204531f799e0c6"
+    instance_type = "t3.micro"
+    security_groups = [aws_security_group.key_security_group.name]
+    key_name = aws_key_pair.sshkey.key_name
+    tags = {
+        Name = "key_server"
+    }
+    
+    provisioner "local-exec" {
+        command = "sleep 30 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${self.public_ip},' -u ec2-user --private-keyydeploy.yml"
+    }
+}
+output "instance_public_ip" {
+    value = aws_instance.key_server.public_ip
+  
+}
